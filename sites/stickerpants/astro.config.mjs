31bd9bed344fa4a87e2fefa610efdf26422b21f6
Tauqeer Ahmed <tauqeer1040@gmail.com>
2026-09-21 15:46:52 +0530

    feat: deploy landing to Cloudflare Workers + custom domain

--- sites/stickerpants/astro.config.mjs
+++ sites/stickerpants/astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: 'https://stickerpants.taucity.xyz',
   trailingSlash: 'ignore',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare()
 });
